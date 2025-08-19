cc73a188dc1b34ec5a1fa7a5bc60a9a9a9fad2b5
Jonathan <67755045+tracert0nexus@users.noreply.github.com>
2025-08-19 17:05:54 +0200

    Update messageHistory.js

--- demo/src/messageHistory.js
+++ demo/src/messageHistory.js
@@ -5,7 +5,7 @@ export default [
     id: 21,
     data: {text: `Hallo liebe:r Unisportler:in - wie kann ich dir weiterhelfen?`},
     suggestions: [
-      { 'Ich möchte Informationen zum Unisportausweis': 1 },
+      { 'Ich moechte Informationen zum Unisportausweis': 1 },
       { 'Angebotsanmeldung mit der eduID (SWITCH)': 10 }
     ]
   }
